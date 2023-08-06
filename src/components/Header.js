1bdd8455262f51c71132b6d0c17301fcd70b81ef
Vikas Kumar <=>
2023-08-06 15:18:17 +0530

    active link highlight

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,18 @@ import { IoMdCall } from 'react-icons/io';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import Colors from '../utils/ColorUtils';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const [showContent, setShowContent] = useState(false);
+  const location = useLocation();
 
   const toggleContent = () => {
     setShowContent(!showContent);
+  };
+
+  const isActiveRoute = (route) => {
+    return location.pathname === route;
   };
 
   return (
@@ -26,22 +31,22 @@ const Header = () => {
           </div>
           <div className="flex items-center space-x-2">
             <BiSolidBookHeart color={Colors.green} size={30} />
-            <h2 className="text-xl font-bold header-link">
+            <h2 className={`text-xl font-bold header-link ${isActiveRoute('/') ? 'active' : ''}`}>
                 <Link to="/">Bict Academy</Link>
             </h2>
           </div>
         </div>
       </div>
       <div className={`sm:flex flex-col items-center space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 ${showContent ? 'block' : 'hidden'}`}>
-        <p className="header-link text-center sm:text-left">
+        <p className={`header-link text-center sm:text-left ${isActiveRoute('/about') ? 'active' : ''}`}>
             <Link to="/about">
                 About Us
             </Link>
         </p>
-        <p className="header-link text-center sm:text-left">
+        <p className={`header-link text-center sm:text-left ${isActiveRoute('/courses') ? 'active' : ''}`}>
             <Link to="/courses">Our Courses</Link>
         </p>
-        <p className="header-link text-center sm:text-left">
+        <p className={`header-link text-center sm:text-left ${isActiveRoute('/teachers') ? 'active' : ''}`}>
             <Link to="/teachers">Our Teachers</Link>
         </p>
         <p className="flex items-center header-link text-center sm:text-left">
